9bc911576fb3b39d05815d83f9dc2e573ad9bbf5
Pawel Piatek <pawelpiatek98@gmail.com>
2021-03-29 17:53:13 +0200

    added basic styles to training plans

--- src/components/Trainings/TrainingPlans/TrainingPlans.tsx
+++ src/components/Trainings/TrainingPlans/TrainingPlans.tsx
@@ -1,12 +1,16 @@
 import React, {useContext} from 'react';
-import styled from 'styled-components';
+import {
+    TrainingPlansWrapper,
+    TrainingPlansHeading,
+    TrainingPlansHeader
+} from './TrainingPlansStyles';
+import * as variables from '../../../assets/styles/variables';
+import {PrimaryButton} from '../../../assets/styles/customStylesComponents/buttons';
 import {TrainingPlanContext, TrainingPlanIntro} from "../../../contexts/TrainingPlansContext";
 import {Link} from 'react-router-dom';
 import CreateNewPlanForm from "./CreateNewPlanForm/CreateNewPlanForm";
 
-const TrainingPlansWrapper = styled.div`
 
-`;
 
 interface Props {
     validationSchema: any;
@@ -16,7 +20,11 @@ const TrainingPlans: React.FC<Props> = ({validationSchema}) => {
     const {trainingPlans} = useContext(TrainingPlanContext);
     return (
         <TrainingPlansWrapper>
-            <h1>Plans</h1>
+            <TrainingPlansHeading>
+                <TrainingPlansHeader>TrainingPlans</TrainingPlansHeader>
+                <PrimaryButton color={variables.yellowPrimary}>New</PrimaryButton>
+            </TrainingPlansHeading>
+
             <CreateNewPlanForm validationSchema={validationSchema}/>
             {trainingPlans.map(plan => (
                 <Link to={{pathname: `/trainings/training-plans/${plan._id}`, state: plan._id}}  key={plan._id} >
---
+++ src/components/Trainings/TrainingPlans/TrainingPlansStyles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import * as variables from '../../../assets/styles/variables'
+
+export const TrainingPlansWrapper = styled.div`
+  padding: 0  3rem;
+`;
+
+export const TrainingPlansHeading = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+`;
+
+export const TrainingPlansHeader = styled.h1`
+  font-weight: 400;
+  font-size: ${variables.textHeader};
+  margin-right: 5rem;
+`;
