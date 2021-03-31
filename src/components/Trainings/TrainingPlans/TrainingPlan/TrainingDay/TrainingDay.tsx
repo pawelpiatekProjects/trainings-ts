2e547fffd30beece4c7f0f234fcabd7d9b73f7d1
Pawel Piatek <pawelpiatek98@gmail.com>
2021-03-31 15:24:13 +0200

    improved adding new training days

--- src/components/Trainings/TrainingPlans/TrainingPlan/TrainingDay/TrainingDay.tsx
+++ src/components/Trainings/TrainingPlans/TrainingPlan/TrainingDay/TrainingDay.tsx
@@ -5,35 +5,50 @@ import {
     TrainingDayHeader,
     ExercisesWrapper,
     TopButtons,
-    TopButton
+    TopButton,
+    EmptyExercisesWrapper,
+    EmptyExercisesHeader,
+    Button
 } from './TrainingDayStyles';
 import {TrainingDay as TrainingDayType} from "../../../../../contexts/TrainingPlansContext";
 import Exercise from "./Exercise/Exercise";
-
-
-
+import * as variables from '../../../../../assets/styles/variables';
+import {PopUpContext} from "../../../../../contexts/PopUpContext";
 
 interface Props {
     trainingDay: TrainingDayType;
 }
 
 const TrainingDay: React.FC<Props> = ({trainingDay}) => {
-    console.log(trainingDay);
+    console.log('exercises: ',trainingDay.exercises);
+    const {onOpenModal} = useContext(PopUpContext);
+
+    let result;
+    if(trainingDay.exercises.length <= 0) {
+        result = (
+            <EmptyExercisesWrapper>
+                <EmptyExercisesHeader>Your exercises list is empty. Add new training day</EmptyExercisesHeader>
+                <Button color={variables.yellowPrimary}>Add</Button>
+            </EmptyExercisesWrapper>
+        )
+    } else {
+        result = trainingDay.exercises.map(exercise => (
+            <Exercise exercise={exercise} key={exercise._id}/>
+        ))
+    }
 
     return (
         <TrainingDayWrapper>
             <TrainingDayHeading>
                 <TrainingDayHeader>{trainingDay.trainingDayName}</TrainingDayHeader>
                 <TopButtons>
+                    <TopButton>Add exercise</TopButton>
                     <TopButton>Edit</TopButton>
                     <TopButton>Delete</TopButton>
                 </TopButtons>
             </TrainingDayHeading>
-
             <ExercisesWrapper>
-                {trainingDay.exercises.map(exercise => (
-                    <Exercise exercise={exercise} key={exercise._id}/>
-                ))}
+                {result}
             </ExercisesWrapper>
         </TrainingDayWrapper>
     )
--- src/components/Trainings/TrainingPlans/TrainingPlan/TrainingDay/TrainingDayStyles.ts
+++ src/components/Trainings/TrainingPlans/TrainingPlan/TrainingDay/TrainingDayStyles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import * as variables from '../../../../../assets/styles/variables';
+import {PrimaryButton} from '../../../../../assets/styles/customStylesComponents/buttons';
 
 export const TrainingDayWrapper = styled.div`
   background: ${variables.grayPrimary};
@@ -15,13 +16,13 @@ export const TrainingDayHeading = styled.div`
 `;
 
 export const TrainingDayHeader = styled.h1`
-  width: 90%;
+  width: 80%;
   text-align: center;
   font-weight: 400;
 `;
 
 export const TopButtons = styled.div`
-  width: 10%;
+  width: 20%;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -46,4 +47,23 @@ export const ExercisesWrapper = styled.div`
   background: ${variables.light};
   padding: 2rem;
 `;
+
+export const EmptyExercisesWrapper = styled.div`
+  width: 100%;
+  border: 1px dashed ${variables.textColorTertiary};
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EmptyExercisesHeader = styled.h1`
+  color: ${variables.textColorTertiary};
+`;
+
+export const Button = styled(PrimaryButton)`
+
+`;
+
 
